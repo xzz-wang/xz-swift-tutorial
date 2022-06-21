a1b8634c6ab8d874d7e94e108afea1d397634cc3
Xuezheng Wang <xzzwang@icloud.com>
2022-06-21 11:54:35 -0700

    Added homework1.

---
+++ homework/homework1.swift
@@ -0,0 +1,35 @@
+
+// Question 0 
+// 理解所有的代码
+
+// Question 1
+// 任务：将华氏度转化为摄氏度
+// 提示：华氏度减去32之后再除以1.8，我们就能得到摄氏度。
+print("Question 1")
+let temperatureInF1: Double = 96.8
+let temperatureInC1 = "错误" // TODO: 把赋值的右边改成正确的公式
+print("\(temperatureInF1)华氏度在摄氏度下是\(temperatureInC1)")
+
+
+
+
+// Question 2
+// 任务：将摄氏度转化为华氏度，最后显示一个整数结果
+// 提示：华氏度等于摄氏度乘以1.8之后再加32
+print("Question 2")
+let temperatureInC2: Double = 36.0
+let temperatureInF2 = "错误" // TODO: 把赋值的右边改成正确的公式
+print("\(temperatureInC2)摄氏度在华氏度下是\(temperatureInF2)")
+
+
+
+
+// Question3
+// 任务：将一下这句话的作者，内容，和字符串的长度print出来。
+// 示例输出：xxx曾说过："xxxxxx" 这句话一共有xxx个字符。
+// 提示：在字符串字面量中使用双引号需要用到转义字符（反斜杠）
+print("Question 3")
+let author = "鲁迅"
+let text = "墙外有两棵树，一棵是枣树，另一棵也是枣树。"
+// TODO: 把结果print出来。
+
