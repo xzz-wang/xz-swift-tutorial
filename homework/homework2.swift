525e191e5e1ceb75b1c9c748fcf5baef674e1d2e
Xuezheng Wang <xzzwang@icloud.com>
2022-07-23 23:13:05 -0700

    Added code for homework2

---
+++ homework/homework2.swift
@@ -0,0 +1,37 @@
+// 作业2: 条件语句 Bool 函数
+
+// Q1. 写一个函数检查一个类型为String的用户名。这个用户名长度必须在[最小值, 最大值]的范围内。如果符合这个要求，函数返回true；不符合则返回false。
+let minLength = 3
+let maxLength = 10
+
+// 你的函数写在这里
+
+
+
+
+
+// Q2. 写一个函数，把考试成绩从分数转换成等级。
+// 输入：Int 输出: String
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// 0-59: F
+// 其他: N/A
+
+// 你的函数写在这里
+
+
+
+
+// Q3. 计算一个物体在各个行星上的重量
+// "earth": 1.0
+// "mars": 0.38
+// "venus": 0.91
+// "mercury": 0.38
+// Example: weightOn(planet: "mars", mass: 1.0) == 0.38
+func weightOn(planet: String, mass: Double) -> Double {
+    // 补全这个函数
+    return 0.0
+}
+
