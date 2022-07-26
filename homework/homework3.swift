9422031f2ebb8d3d6aa76ed7b6bda05ad974af9d
Xuezheng Wang <xzzwang@icloud.com>
2022-07-26 16:52:01 -0700

    Added code for homework3

---
+++ homework/homework3.swift
@@ -0,0 +1,59 @@
+/*
+ Q1: 写一个函数，将一段字符串重复打印出来。
+ 例子: repeatPrint(content: "你好", count: 3)
+ 输出:
+ 你好
+ 你好
+ 你好
+ */
+func repeatPrint(content: String, count: Int) {
+    // 补全这个函数
+}
+
+//repeatPrint(content: "你好", count: 5)
+
+
+
+/*
+ Q2: 写一个函数。这个函数有一个Int的参数叫height。
+ 根据这个参数在命令行打印出一个相应高度的金字塔。
+ 例子：printPyramid(height: 4)
+ 输出：
+ *
+ **
+ ***
+ ****
+ 提示：print("xxx", terminator: "")可以阻止print自动换行
+*/
+func printPyramid(height: Int) {
+    // 补全这个函数
+}
+//printPyramid(height: 4)
+
+
+
+/*
+ Q3
+ 将一个Array里面的每一个值转换为它的绝对值
+ 例子：arrayAbs(input: [0, -1, 2, -3, 4, -5]) == [0, 1, 2, 3, 4, 5]
+ */
+func arrayAbs(input: [Int]) -> [Int] {
+    // 补全这个函数
+    return []
+}
+
+print(arrayAbs(input: [0, -1, 2, -3, 4, -5]))
+
+
+
+/*
+ Q4
+ 斐波那契数列：从1开始，将所有小于max的斐波那契数列的项按顺序打印出来
+ 例子：fibonacci(max: 30)
+ 输出：1 1 2 3 5 8 13 21
+ */
+func fibonacci(max: Int) {
+    // 补全这个函数
+}
+
+//fibonacci(max: 30)
